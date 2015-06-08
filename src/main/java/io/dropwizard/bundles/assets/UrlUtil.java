e3ef9fce60e3b261aff5715eeb0b52d86fbf35a0
rvs-fluid-it <koen.roevens@gmail.com>
2015-06-08 11:56:31 +0200

    Updated javadoc

--- src/main/java/io/dropwizard/bundles/assets/UrlUtil.java
+++ src/main/java/io/dropwizard/bundles/assets/UrlUtil.java
@@ -1,17 +1,7 @@
 package io.dropwizard.bundles.assets;
 
-import io.dropwizard.servlets.assets.ResourceNotFoundException;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.JarURLConnection;
 import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.zip.ZipEntry;
 
 public class UrlUtil {
   /*
@@ -20,12 +10,10 @@ public class UrlUtil {
   private static final String JAR_URL_SEPARATOR = "!/";
 
   /**
-   * Returns true if the URL passed to it corresponds to a directory.
-   * This is slightly tricky due to some quirks of the {@link java.util.jar.JarFile} API.
-   * Only jar:// and file:// URLs are supported.
+   * Switch zip protocol (used by Weblogic) to jar protocol (supported by DropWizard).
    *
-   * @param resourceUrl the URL to check
-   * @return true if resource is a directory
+   * @param resourceUrl the URL to switch protocol eventually
+   * @return the URL with eventually switched protocol
    */
   public static URL switchFromZipToJarProtocolIfNeeded(URL resourceUrl)
       throws MalformedURLException {
@@ -41,6 +29,4 @@ public class UrlUtil {
     }
     return resourceUrl;
   }
-
-
 }
